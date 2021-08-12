ca09e40acfd6ea3d3a71afdabab3784ca8ac1e98
charan_kanduri <charan2000kanduri@gmail.com>
2021-08-13 01:46:41 +0530

    [CHARAN]2-AddedEmployeeToJsonDB

--- RESTSharpTestingProject/RESTSharpApiTestCases.cs
+++ RESTSharpTestingProject/RESTSharpApiTestCases.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System.Collections.Generic;
 using System.Net;
@@ -31,7 +32,25 @@ namespace RESTSharpTestingProject
             IRestResponse response = getEmployeeList();
             Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
             List<EmployeeAttributes> employeeAttributesList = JsonConvert.DeserializeObject<List<EmployeeAttributes>>(response.Content);
-            Assert.AreEqual(6, employeeAttributesList.Count);
+            Assert.AreEqual(7, employeeAttributesList.Count);
         }
+        [TestMethod]
+        public void OnCalling_AddPersonToJsonDB()
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            JObject jsonObjectBody = new JObject();
+            jsonObjectBody.Add("id", "8");
+            jsonObjectBody.Add("firstName", "Charan");
+            jsonObjectBody.Add("lastName", "Kanduri");
+            jsonObjectBody.Add("salary", "2400");
+
+            request.AddParameter("application/json", jsonObjectBody, ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+            EmployeeAttributes employeeAttributes = JsonConvert.DeserializeObject<EmployeeAttributes>(response.Content);
+            Assert.AreEqual("Charan", employeeAttributes.firstName);
+        }        
     }
 }
